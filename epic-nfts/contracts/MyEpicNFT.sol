df3da8f60073961a9fd30f455890540252ee8503
Prathmesh Aghao <prathmeshaghao@kccemsr.edu.in>
2023-07-18 23:47:29 +0530

    committed

--- epic-nfts/contracts/MyEpicNFT.sol
+++ epic-nfts/contracts/MyEpicNFT.sol
@@ -1,11 +1,146 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.1;
 
+// We first import some OpenZeppelin Contracts.
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract MyEpicNFT {
-    constructor() {
-        console.log("This is my NFT contract. Whoa!");
-    }
+import { Base64 } from "./libraries/Base64.sol";
+
+// We inherit the contract we imported. This means we'll have access
+// to the inherited contract's methods.
+contract MyEpicNFT is ERC721URIStorage {
+  // Magic given to us by OpenZeppelin to help us keep track of tokenIds.
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+// This is our SVG code. All we need to change is the word that's displayed. Everything else stays the same.
+  // So, we make a baseSvg variable here that all our NFTs can use.
+  string svgPartOne = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='";
+  string svgPartTwo = "'/><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
+  // I create three arrays, each with their own theme of random words.
+  // Pick some random funny words, names of anime characters, foods you like, whatever! 
+  string[] firstWords = ['Red', 'Blue', 'Green', 'Yellow', 'Orange', 'Purple', 'Pink', 'Brown', 'Black', 'White', 'Gray', 'Cyan', 'Magenta', 'Teal', 'Maroon', 'Navy', 'Olive', 'Turquoise', 'Indigo', 'Gold', 'Silver', 'Coral', 'Lavender', 'Salmon', 'Mint'];
+  string[] secondWords = ['Goku', 'Naruto', 'Luffy', 'Ichigo', 'Spike', 'Light', 'Kirito', 'Sasuke', 'Vegeta', 'Zoro', 'Gon', 'Killua', 'Eren', 'Levi', 'Kakashi', 'Itachi', 'Saitama', 'Shinji', 'Misaka', 'Ryuk', 'Guts', 'Sora', 'Alucard', 'Edward', 'Mugen'];
+  string[] thirdWords = ['PacMan', 'Tetris', 'FIFA', 'Sudoku', 'Monopoly', 'Scrabble', 'Jenga', 'Chess', 'Checkers', 'Clue', 'Uno', 'Catan', 'Risk', 'Splatoon', 'Portal', 'Braid', 'Limbo', 'Othello', 'Qwirkle', 'Santorini', 'Azul', 'Carcassonne', 'Pandemic', 'Codenames', 'TicketRide'];
+
+  string[] colors = ["red", "#08C2A8", "black", "yellow", "blue", "green"];
+
+  event NewEpicNFTMinted(address sender, uint256 tokenId);
+
+  // We need to pass the name of our NFTs token and its symbol.
+  constructor() ERC721 ("SquareNFT", "SQUARE") {
+    console.log("This is my NFT contractt");
+  }
+
+  // I create a function to randomly pick a word from each array.
+  function pickRandomFirstWord(uint256 tokenId) public view returns (string memory) {
+    // I seed the random generator. More on this in the lesson. 
+    uint256 rand = random(string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId))));
+    // Squash the # between 0 and the length of the array to avoid going out of bounds.
+    rand = rand % firstWords.length;
+    return firstWords[rand];
+  }
+
+  function pickRandomSecondWord(uint256 tokenId) public view returns (string memory) {
+    uint256 rand = random(string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId))));
+    rand = rand % secondWords.length;
+    return secondWords[rand];
+  }
+
+  function pickRandomThirdWord(uint256 tokenId) public view returns (string memory) {
+    uint256 rand = random(string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId))));
+    rand = rand % thirdWords.length;
+    return thirdWords[rand];
+  }
+
+   function pickRandomColor(uint256 tokenId) public view returns (string memory) {
+    uint256 rand = random(string(abi.encodePacked("COLOR", Strings.toString(tokenId))));
+    rand = rand % colors.length;
+    return colors[rand];
+  }
+
+  function random(string memory input) internal pure returns (uint256) {
+      return uint256(keccak256(abi.encodePacked(input)));
+  }
+
+  // A function our user will hit to get their NFT.
+  function makeAnEpicNFT() public {
+     // Get the current tokenId, this starts at 0.
+    uint256 newItemId = _tokenIds.current();
+
+    // We go and randomly grab one word from each of the three arrays.
+    string memory first = pickRandomFirstWord(newItemId);
+    string memory second = pickRandomSecondWord(newItemId);
+    string memory third = pickRandomThirdWord(newItemId);
+    string memory combinedWord = string(abi.encodePacked(first, second, third));
+
+    // Add the random color in.
+    string memory randomColor = pickRandomColor(newItemId);
+    string memory finalSvg = string(abi.encodePacked(svgPartOne, randomColor, svgPartTwo, combinedWord, "</text></svg>"));
+
+    // Get all the JSON metadata in place and base64 encode it.
+    string memory json = Base64.encode(
+        bytes(
+            string(
+                abi.encodePacked(
+                    '{"name": "',
+                    // We set the title of our NFT as the generated word.
+                    combinedWord,
+                    '", "description": "A highly acclaimed collection of squares.", "image": "data:image/svg+xml;base64,',
+                    // We add data:image/svg+xml;base64 and then append our base64 encode our svg.
+                    Base64.encode(bytes(finalSvg)),
+                    '"}'
+                )
+            )
+        )
+    );
+
+    // Just like before, we prepend data:application/json;base64, to our data.
+    string memory finalTokenUri = string(
+        abi.encodePacked("data:application/json;base64,", json)
+    );
+
+    console.log("\n--------------------");
+    console.log(
+    string(
+        abi.encodePacked(
+            "https://nftpreview.0xdev.codes/?code=",
+            finalTokenUri
+          )
+        )
+      );
+    console.log("--------------------\n");
+
+     // Actually mint the NFT to the sender using msg.sender.
+    _safeMint(msg.sender,newItemId);
+    
+    // Update your URI!!!
+    _setTokenURI(newItemId, finalTokenUri);
+
+    _setTokenURI(newItemId, "ipfs:QmZd8KwXtTLVjYTPCujEEffCcFLMJoYUJWTLrqT7U1fude");
+    
+  
+    // Increment the counter for when the next NFT is minted.
+      _tokenIds.increment();
+      console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+
+      emit NewEpicNFTMinted(msg.sender, newItemId);
+  }
+
+  // Set the NFT's metadata
+  // function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+  // require(_exists(_tokenId));
+  // console.log("An NFT w/ ID %s has been minted to %s", _tokenId, msg.sender);
+  // return string(
+  //       abi.encodePacked(
+  //           "data:application/json;base64,",
+  //           "ewogICAgIm5hbWUiOiAiQWdoYW8gUG9yZ2EiLAogICAgImRlc2NyaXB0aW9uIjogIkJlc3QgTkZUIGZyb20gdGhlIGhpZ2hseSBhY2NsYWltZWQgc3F1YXJlIGNvbGxlY3Rpb24iLAogICAgImltYWdlIjogImRhdGE6aW1hZ2Uvc3ZnK3htbDtiYXNlNjQsUEhOMlp5QjRiV3h1Y3owaWFIUjBjRG92TDNkM2R5NTNNeTV2Y21jdk1qQXdNQzl6ZG1jaUlIQnlaWE5sY25abFFYTndaV04wVW1GMGFXODlJbmhOYVc1WlRXbHVJRzFsWlhRaUlIWnBaWGRDYjNnOUlqQWdNQ0F6TlRBZ016VXdJajRLSUNBZ0lEeHpkSGxzWlQ0dVltRnpaU0I3SUdacGJHdzZJSGRvYVhSbE95Qm1iMjUwTFdaaGJXbHNlVG9nYzJWeWFXWTdJR1p2Ym5RdGMybDZaVG9nTVRSd2VEc2dmVHd2YzNSNWJHVStDaUFnSUNBOGNtVmpkQ0IzYVdSMGFEMGlNVEF3SlNJZ2FHVnBaMmgwUFNJeE1EQWxJaUJtYVd4c1BTSmliR0ZqYXlJZ0x6NEtJQ0FnSUR4MFpYaDBJSGc5SWpVd0pTSWdlVDBpTlRBbElpQmpiR0Z6Y3owaVltRnpaU0lnWkc5dGFXNWhiblF0WW1GelpXeHBibVU5SW0xcFpHUnNaU0lnZEdWNGRDMWhibU5vYjNJOUltMXBaR1JzWlNJK1FXZG9ZVzhnVUc5eVoyRThMM1JsZUhRK0Nqd3ZjM1puUGc9PSIKfQ=="
+  //       )
+  //   );
+  // }
 }
--- epic-nfts/hardhat.config.js
+++ epic-nfts/hardhat.config.js
@@ -1,4 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
+// require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -10,12 +13,24 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.0",
+  solidity: "0.8.1",
+  networks: {
+    goerli: {
+      url: process.env.QUICKNODE_API_KEY_URL,
+      accounts: [process.env.GOERLI_PRIVATE_KEY],
+    },
+
+    // mainnet: {
+    //   chainId: 1,
+    //   url: process.env.PROD_QUICKNODE_KEY,
+    //   accounts: [process.env.PRIVATE_KEY],
+    // },
+
+    // etherscan: {
+    //   // Your API key for Etherscan
+    //   // Obtain one at https://etherscan.io/
+    //   apikey: process.env.ETHERSCAN_API_KEY,
+    // },
+  },
 };
